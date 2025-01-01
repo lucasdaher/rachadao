405ef6d89b316c3029d35eba6fb23dfd3d14dbd9
lucasdaher <contato@lucasdaher.com>
2025-01-01 17:58:06 -0300

    chore: atualizando estilização do tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        default:
+          "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset",
+      },
       transitionDuration: {
         200: "0.2s",
       },
