a2943875c5ea38c911c9957780413b2a7de1e03a
lucasdaher <contato@lucasdaher.com>
2025-01-02 13:51:14 -0300

    feat: implementando botão de pagamento e checkout stripe.

---
+++ src/components/Payment/components/paymentButton.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { useState, useEffect, useCallback } from "react";
+import { usePayment } from "@/components/Plans/hooks/usePayment";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+type PaymentButtonProps = {
+  children: React.ReactNode;
+  plan: any | undefined | null;
+};
+
+export function PaymentButton({ children, plan }: PaymentButtonProps) {
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [clientSecret, setClientSecret] = useState<string | null>(null);
+  const { paymentOpen, setPaymentOpen } = usePayment();
+
+  useEffect(() => {
+    const loadStripeAsync = async () => {
+      const stripe = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ""
+      );
+      setStripePromise(stripe);
+    };
+
+    loadStripeAsync();
+  }, []);
+
+  const fetchClientSecret = useCallback(async () => {
+    try {
+      const response = await fetch("/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao buscar o client_secret.");
+      }
+
+      const data = await response.json();
+      setClientSecret(data.client_secret);
+    } catch (error) {
+      console.error("Erro ao buscar o client_secret:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (paymentOpen) {
+      fetchClientSecret();
+    }
+  }, [paymentOpen, fetchClientSecret]);
+
+  const options = {
+    clientSecret,
+  };
+
+  return (
+    <React.Fragment>
+      {paymentOpen && (
+        <div className="bg-black-900 bg-opacity-70 fixed top-0 left-0 min-h-screen w-full z-10"></div>
+      )}
+      <Dialog modal>
+        <DialogTrigger asChild>
+          <Button
+            variant={plan.name === "Plano Rachadão" ? "default" : "outline"}
+            className={`mt-8 block w-full ${
+              plan.name === "Plano Rachadão"
+                ? "bg-green-600 text-white hover:bg-green-500"
+                : ""
+            }`}
+            onClick={() => {
+              setPaymentOpen(true);
+            }}
+          >
+            {children}
+          </Button>
+        </DialogTrigger>
+        <DialogContent onCloseAutoFocus={() => setPaymentOpen(false)}>
+          <>
+            <VisuallyHidden.Root>
+              <DialogTitle>Plano de Assinatura</DialogTitle>
+            </VisuallyHidden.Root>
+            {stripePromise && (
+              <EmbeddedCheckoutProvider
+                stripe={stripePromise}
+                options={options}
+              >
+                <EmbeddedCheckout />
+              </EmbeddedCheckoutProvider>
+            )}
+          </>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
+  );
+}
---
+++ src/components/Payment/components/paymentConfirmation.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function PaymentConfirmation() {
+  return <React.Fragment></React.Fragment>;
+}
