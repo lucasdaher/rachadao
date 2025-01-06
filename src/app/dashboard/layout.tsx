a37ac519a9f7cb08580efa00f02fa0f2060d448d
lucasdaher <contato@lucasdaher.com>
2025-01-05 21:43:17 -0300

    feat: adicionando sidebar na dashboard.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { auth } from "@/../auth";
 import { redirect } from "next/navigation";
+import { SidebarProvider } from "@/components/Sidebar/contexts/SidebarContext";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
+  // const session = await auth();
 
-  if (!session) {
-    redirect(`/signin`);
-  }
+  // if (!session) {
+  //   redirect(`/signin`);
+  // }
 
-  return <div className="">{children}</div>;
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen w-full fixed top-0 left-0 bg-black-900">
+        {children}
+      </div>
+    </SidebarProvider>
+  );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import React from "react";
-import logoutAction from "../(auth)/logout/logoutAction";
+
+import { Sidebar } from "@/components/Sidebar";
+import Image from "next/image";
+
+import { LogOut, X } from "lucide-react";
+import Link from "next/link";
+import { useSidebar } from "@/components/Sidebar/contexts/SidebarContext";
 
 export default function DashboardPage() {
+  const { isOpen, toggleSidebar } = useSidebar();
   return (
     <React.Fragment>
-      <div className="bg-white">
-        <section>Dashboard está aqui</section>
-        <Button onClick={logoutAction}>Logout</Button>
-      </div>
+      <Sidebar />
     </React.Fragment>
   );
 }
